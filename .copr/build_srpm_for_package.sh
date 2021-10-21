1786ef4209e05f2d843afc80810a42bb055db4ba
Damien Ciabrini <damien.ciabrini@gmail.com>
2021-10-21 17:46:10 +0200

    Rework the repo to only use 'make srpm' in COPR

--- .copr/build_srpm_for_package.sh
+++ .copr/build_srpm_for_package.sh
@@ -2,14 +2,19 @@
 set -e
 
 OUTDIR=$1
+PKG=$2
 
+if [ -z "$PKG" ]; then
 # emudbg-0.2+202105012058-1
 tag=$(git tag --sort=-creatordate | grep -E '[^0-9]*-[0-9.]*\+[0-9]*-[0-9]*' | head -1)
 pkg=$(echo $tag | sed -E 's%^([^0-9]*)-[0-9].*$%\1%')
+else
+pkg=$(echo $PKG | sed 's/.spec//')
+tag=$(git tag --sort=-creatordate | grep "$pkg" | head -1)
+fi
 
 echo "Build srpm for package $pkg ($tag)"
 
-cd $pkg
 spectool -g $pkg.spec
 rpmbuild -bs --define "_sourcedir $PWD" --define "_srcrpmdir $PWD" $pkg.spec
 mv -v *.src.rpm $OUTDIR
